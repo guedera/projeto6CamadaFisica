ce31571996f8f3d310a8f9d32df87c391ef1c328
guedera <guilhermegg5@al.insper.edu.br>
2025-04-10 19:11:57 -0300

    receptor logica sem nada

---
+++ Codes/Receptor/receptor.py
@@ -0,0 +1,15 @@
+#capta sinal
+
+#checka volume
+
+#fourier do sinal
+
+#identifica picos! (minimo de 5 picos)
+
+#tabela de acordes cadastrados
+
+#identifica acorde
+
+#plota grafico no tempo do sinal recebido
+
+#plota o gráfico da transformada do sinal
